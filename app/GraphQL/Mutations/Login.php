a74d9d56a4b775c167467ce5cfa8297446a8674d
Ralph <ralph@huwiler.dev>
2020-02-07 20:51:12 +0100

    added new airlock login

---
+++ app/GraphQL/Mutations/Login.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use Hash;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Illuminate\Support\Facades\Auth;
+
+class Login
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param null $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param mixed[] $args The arguments that were passed into the field.
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext $context Arbitrary data that is shared between all fields of a single query.
+     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $user = User::where('email', $args['email'])->first();
+
+        if (! $user || ! Hash::check($args['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return [
+            'access_token' => $user->createToken('device_name')->plainTextToken,
+            'user' => $user
+        ];
+    }
+}
--- tests/GraphQL/AuthTest.php
+++ tests/GraphQL/AuthTest.php
@@ -7,6 +7,43 @@ use Tests\GraphQLTestCase;
 
 class AuthTest extends GraphQLTestCase
 {
+    public function test_a_user_can_login()
+    {
+        $user = app(\App\Factories\UserFactory::class)->make();
+        $user->save();
+
+        $token = $this->postGraphQL([
+            'query' => '
+                mutation login($data: LoginInput) {
+                    login(data: $data) {
+                        token
+                    }
+                }
+            ',
+            'variables' => [
+                'data' => [
+                    'email' => $user->email,
+                    'password' => 'password',
+                ]
+            ],
+        ])->decodeResponseJson('data.login.token');
+
+        $this->postGraphQL([
+            'query' => '{
+                me {
+                    id
+                }
+            }'],
+            ['Authorization' => "Bearer $token"]
+        )->assertJson([
+            'data' => [
+                'me' => [
+                    'id' => $user->id,
+                ]
+            ]
+        ]);
+    }
+
     public function test_that_me_returns_the_active_user()
     {
         $user = app(\App\Factories\UserFactory::class)->create();
