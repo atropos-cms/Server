85cb3f442ffbe9990fd45f6d0f324daee976ef34
Ralph <ralph@huwiler.dev>
2020-02-14 20:48:23 +0100

    added category to permissions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Create a tenant
         $this->call(TenantSeeder::class);
+
+        // These seeders apply to the tenant just created
         $this->call(UsersTableSeeder::class);
         $this->call(PagesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
