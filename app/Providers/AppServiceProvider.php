9aa1010094ca6523d6ee95dec239f32afd1e7c14
Ralph <ralph@huwiler.dev>
2020-02-14 20:57:10 +0100

    ensure permissions are cached per tenant

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // spatie/laravel-permission caches permissions & roles to save DB queries,
+        // which means that we need to separate the permission cache by tenant.
+        tenancy()->hook('bootstrapped', function (TenantManager $tenantManager) {
+            \Spatie\Permission\PermissionRegistrar::$cacheKey = 'spatie.permission.cache.tenant.' . $tenantManager->getTenant('id');
+        });
     }
 }
